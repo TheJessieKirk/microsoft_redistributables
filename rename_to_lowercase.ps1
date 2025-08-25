823e4d99976bec893fa2f731831ec2c22909fbfd
Jessie Kirk <jessiekirk@jessiekirk.xyz>
2025-08-25 13:11:40 +0100

    Added PowerShell script to rename files to lowercase.

---
+++ rename_to_lowercase.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem -LiteralPath $PSScriptRoot -Recurse | Where {
+  $_.Name -cne $_.Name.ToLower()
+} | ForEach-Object {
+  $tn="$($_.Name)-temp";
+  $tfn="$($_.FullName)-temp";
+  $nn=$_.Name.ToLower();
+  Rename-Item -LiteralPath $_.FullName -NewName $tn;
+  Rename-Item -Path $tfn -NewName $nn -Force;
+  Write-Host "New Name: $($nn)";
+}
